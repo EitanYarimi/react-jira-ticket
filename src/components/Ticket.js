a77e8f5de655b796cb59e754dca7034f5b83b09f
Eitan Yarimi <eitanyarimi@gmail.com>
2021-03-20 12:47:06 +0200

    ui fix

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -4,6 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import CheckIcon from '@material-ui/icons/Check';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import TicketForm from './TicketForm';
 import {styles} from '../theme'
@@ -12,8 +14,6 @@ import { TicketTypes } from '../common/TickeTypes'
 import { ProjectService, ProjectServiceMapper } from '../api/ProjectsService';
 import { InlineIcon} from '@iconify/react';
 import editIcon from '@iconify-icons/akar-icons/edit';
-import CheckIcon from '@material-ui/icons/Check';
-import Box from '@material-ui/core/Box';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 
@@ -68,42 +68,45 @@ class Ticket extends React.Component {
           </Typography>
         </Toolbar>
       </AppBar>
+      
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography component="h3" variant="h5" align="left">
-           <span> <InlineIcon icon={editIcon} />Create Ticket</span>  
-          </Typography>
-          <BottomNavigation
-              value={this.state.ticketType}
-              onChange={this.onTicketTypeClick}
-              showLabels
-              className={classes.root}
-            >
-              <BottomNavigationAction value={TicketTypes.JIRA} label="Jira"  />
-              <BottomNavigationAction value={TicketTypes.MONDAY} label="Monday"/>
-              <BottomNavigationAction value={TicketTypes.SLACK} label="Slack" />
-            </BottomNavigation>
-          <TicketForm
-            ref={this.ticketFormRef}
-            ticketType={this.state.ticketType}
-            projects={this.state.projects}
-          ></TicketForm>
-          { !this.state.formSubmited &&
-            <div className={classes.buttons}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={this.handleCreateTicket}
-                className={classes.button}
+          {!this.state.formSubmited &&
+            <React.Fragment>
+            <Typography component="h3" variant="h5" align="left">
+            <span> <InlineIcon icon={editIcon} />Create Ticket</span>  
+            </Typography>
+            <BottomNavigation
+                value={this.state.ticketType}
+                onChange={this.onTicketTypeClick}
+                showLabels
+                className={classes.root}
               >
-                Create a Ticket
-            </Button>
-            </div>
+                <BottomNavigationAction value={TicketTypes.JIRA} label="Jira"  />
+                <BottomNavigationAction value={TicketTypes.MONDAY} label="Monday"/>
+                <BottomNavigationAction value={TicketTypes.SLACK} label="Slack" />
+              </BottomNavigation>
+            <TicketForm
+              ref={this.ticketFormRef}
+              ticketType={this.state.ticketType}
+              projects={this.state.projects}
+            ></TicketForm>
+            
+              <div className={classes.buttons}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.handleCreateTicket}
+                  className={classes.button}
+                >
+                  Create a Ticket
+              </Button>
+              </div>
+          </React.Fragment>
           }
-          
           {this.state.formSubmited &&
             <Box color="success.main">
-              <Typography component="h5" variant="h6" align="right">
+              <Typography component="h5" variant="h6" align="left">
                 <span> <InlineIcon icon={CheckIcon} />Ticket Submitted</span>  
               </Typography>
             </Box>
